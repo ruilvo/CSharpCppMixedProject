843a27a92f6dfb05462a81e7db1291bba2f271e9
Rui Oliveira <ruimail24@gmail.com>
2024-11-21 21:58:01 +0000

    Add P/Invoke wrapper for native library methods in NativeCode.cs

---
+++ managed/AetherExample/NativeCode.cs
@@ -0,0 +1,10 @@
+using System.Runtime.InteropServices;
+
+namespace AetherExample
+{
+    public static class NativeMethods
+    {
+        [DllImport("aether_lib", CallingConvention = CallingConvention.Cdecl, EntryPoint = "add")]
+        public static extern int Add(int param);
+    }
+}
