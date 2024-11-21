287e8fda34ef610755eecf4dfb9b729f16f420be
Rui Oliveira <ruimail24@gmail.com>
2024-11-21 20:00:02 +0000

    Add CMake configuration for aether library and header files

---
+++ native/lib/include/aether/aether.hpp
@@ -0,0 +1,16 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * SPDX-FileCopyrightText: 2024 Rui Oliveira <ruimail24@gmail.com>
+ */
+
+#pragma once
+
+#include "aether/api.h"
+
+namespace aether
+{
+
+AETHER_API int sum(int x, int y);
+
+} // namespace aether
