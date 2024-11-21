dc2bbeb520a1b213583d0c14f3281800bac1d170
Rui Oliveira <ruimail24@gmail.com>
2024-11-21 20:08:32 +0000

    Add initial implementation of aether library with source files

---
+++ native/lib/src/aether.cpp
@@ -0,0 +1,17 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * SPDX-FileCopyrightText: 2024 Rui Oliveira <ruimail24@gmail.com>
+ */
+
+#include "aether/aether.hpp"
+
+namespace aether
+{
+
+AETHER_API int sum(int x, int y)
+{
+    return x + y;
+}
+
+} // namespace aether
