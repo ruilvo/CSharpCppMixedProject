edea5d7edd89157bd478bd8351bed755a5e40826
Rui Oliveira <ruimail24@gmail.com>
2024-11-21 22:14:04 +0000

    Update P/Invoke method signature and demonstrate usage in Program.cs

--- managed/AetherExample/NativeCode.cs
+++ managed/AetherExample/NativeCode.cs
@@ -5,6 +5,6 @@ namespace AetherExample
     public static class NativeMethods
     {
         [DllImport("aether_lib", CallingConvention = CallingConvention.Cdecl, EntryPoint = "add")]
-        public static extern int Add(int param);
+        public static extern int Add(int x, int y);
     }
 }
